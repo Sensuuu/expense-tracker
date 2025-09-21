ba8cbf4981abd7179700c110e6ce85aee2a0dc9a
Anurag Giri <sensation1607@gmail.com>
2025-09-21 11:23:01 +0530

    first commit

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, no authorization header" });
+  }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, invalid token format" });
+  }
+
+  let token = req.headers.authorization?.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ message: "Not authorized, no token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
